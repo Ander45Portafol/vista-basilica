5830986fe1bd230b79b9b3e13fccb6156d08f394
Theguishe <guillermogacc0526@gmail.com>
2023-06-19 04:12:25 -0600

    pagina.vue problems resolved
    ya regreso voy a hacer ejercicio

--- pages/pagina.vue
+++ pages/pagina.vue
@@ -29,9 +29,9 @@
             </div>
         </div>
         <div class="mdprincipal flex-col mt-8 px-8 overflow-hidden">
-            <div class="h-16 w-full rounded-xl flex justify-between items-center content-buttons">
-                <form action="" class="w-3/4 flex items-center h-full mt-4">
-                    <input type="text" class="rounded-lg relative w-2/4 h-12 outline-none" placeholder="Buscar ...">
+            <div class="h-16 w-full rounded-xl flex justify-between items-center content-buttons max-[450px]:flex-wrap">
+                <form action="" class="w-3/4 flex items-center h-full mt-4 max-[500px]:w-full">
+                    <input type="text" class="rounded-lg relative w-2/4 h-12 outline-none max-[800px]:w-full min-w-[200px]" placeholder="Buscar ...">
                     <div class="flex justify-end items-center">
                         <!-- Se le asigna la función para limpiar el buscador al botón -->
                         <button class="absolute mr-4" @click="limpiarBuscador()"><svg width="20px" height="20px"
@@ -43,8 +43,8 @@
                         </button>
                     </div>
                 </form>
-                <div class="buttons flex items-center">
-                    <button class="w-12 h-10 flex items-center justify-center ml-4 rounded-lg">
+                <div class="buttons flex mt-4 mr-[-15px] max-[800px]:mt-4 min-w-[100px] max-[450px]:m-auto max-[450px]:mt-3">
+                    <button class="w-12 h-10 flex items-center justify-center ml-4 rounded-lg max-[800px]:w-8 max-[800px]:h-8 max-[800px]:ml-2">
                         <svg width="28px" height="28px" stroke-width="2.5" viewBox="0 0 24 24" fill="none"
                             xmlns="http://www.w3.org/2000/svg" color="#000000">
                             <path
@@ -76,21 +76,21 @@
                     </button>
                 </div>
             </div>
-            <div class="line bg-slate-800 h-0.5 mt-4 w-full"></div>
-            <p class="font-extrabold text-slate-900 mt-8 ml-4">2<span class="text-gray-500 font-normal ml-2">registros
+            <div class="line bg-slate-800 h-0.5 mt-4 w-full min-w-[200px]"></div>
+            <p class="font-extrabold text-slate-900 mt-8 ml-4 max-[425px]:mt-16">2<span class="text-gray-500 font-normal ml-2">registros
                     encontrados!</span></p>
             <div class="contained-data flex-col">
-                <div class="data-contained flex justify-between mt-4 rounded-xl p-4">
-                    <div class="flex justify-start w-3/4 items-center">
-                        <img src="" class="h-10 w-10 rounded-lg border-2 border-gray-800" />
-                        <div class="datainfo flex-col ml-8">
-                            <p class="font-extrabold text-xl text-salte-900">Página- Inicio</p>
-                            <p class="font-normal text-sm mt-1text-gray-500">Inicio del dashboard de la pagina privada</p>
-                            <p class="font-normal text-sm text-gray-500">Número de página: 1</p>
+                <div class="data-contained flex justify-between mt-4 rounded-xl p-4 max-[400px]:flex-wrap max-[400px]:w-full min-w-[200px]">
+                    <div class="flex justify-start w-3/4 items-center max-[400px]:w-full">
+                        <img src="" class="h-10 w-10 rounded-lg border-2 border-gray-800 max-[400px]:hidden" />
+                        <div class="datainfo flex-col ml-8 max-[400px]:p-0 max-[400px]:w-full max-[400px]:ml-0 max-[400px]:text-center">
+                            <p class="font-extrabold text-xl text-salte-900 max-[750px]:text-[18px]">Página- Inicio</p>
+                            <p class="font-normal text-sm mt-1text-gray-500 max-[750px]:text-[12px]">Inicio del dashboard de la pagina privada</p>
+                            <p class="font-normal text-sm text-gray-500 max-[750px]:text-[12px]">Número de página: 1</p>
                         </div>
                     </div>
-                    <div class="buttons-data flex justify-center items-center">
-                        <button class="h-10 w-10 rounded-md flex items-center justify-center editbtn">
+                    <div class="buttons-data flex justify-center items-center max-[750px]:flex-col max-[400px]:flex-row max-[400px]:m-auto max-[400px]:mt-2">
+                        <button class="h-10 w-10 rounded-md flex items-center justify-center editbtn max-[400px]:mx-4">
                             <svg width="26px" height="26px" stroke-width="2" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg" color="#000000">
                                 <path
@@ -98,7 +98,7 @@
                                     stroke="#C99856" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                             </svg>
                         </button>
-                        <button class="h-10 w-10 rounded-md flex items-center justify-center ml-4 deletebtn">
+                        <button class="h-10 w-10 rounded-md flex items-center justify-center ml-4 deletebtn max-[750px]:ml-0 max-[750px]:mt-2 max-[400px]:mt-0 max-[400px]:mx-4">
                             <svg width="26px" height="26px" viewBox="0 0 24 24" stroke-width="2" fill="none"
                                 xmlns="http://www.w3.org/2000/svg" color="#000000">
                                 <path
