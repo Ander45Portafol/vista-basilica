792d82de137cfcf035efeed1c4d6ca98b588e656
itsRoberOOF <83309227+itsRoberOOF@users.noreply.github.com>
2023-07-14 14:20:40 -0600

    añadiendo plugin para los charts

---
+++ plugins/chartJsPlugin.ts
@@ -0,0 +1,23 @@
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+} from "chart.js";
+export default defineNuxtPlugin((nuxtApp) => {
+    ChartJS.register(
+        Title,
+        Tooltip,
+        Legend,
+        BarElement,
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement
+    );
+});
