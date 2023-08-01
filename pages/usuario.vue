6b004fc74b32e063af4a6e7bd5df168031e60f6a
Ander45Portafol <anderdeco25@gmail.com>
2023-08-01 16:29:03 -0600

    Validaciones en usuarios y en anuncios

--- pages/usuario.vue
+++ pages/usuario.vue
@@ -1,74 +1,70 @@
 <template>
-    <div class="mr-4">
-        <div class="principal mt-6">
-            <!--Componente para cargar el menu superior del formulario-->
-            <MenuUsuarioDashboard />
-            <div class="mdprincipal flex-col mt-8 px-8 overflow-hidden">
-                <div class="h-16 w-full rounded-xl flex justify-between items-center content-buttons max-[450px]:flex-wrap">
-                    <form action="" class="w-3/4 flex items-center h-full mt-4 max-[500px]:w-full">
-                        <input type="text"
-                            class="rounded-lg relative w-2/4 h-12 outline-none max-[800px]:w-full min-w-[200px]"
-                            placeholder="Buscar... (usuario/correo)" v-model="buscar.buscador" @keyup="buscarUsuarios()">
-                        <div class="flex justify-end items-center">
-                            <button class="absolute mr-4"><svg width="20px" height="20px" stroke-width="2"
-                                    viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
-                                    <path
-                                        d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"
-                                        stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    </path>
-                                </svg>
-                            </button>
-                        </div>
-                    </form>
-                    <div
-                        class="buttons flex mt-4 mr-[-15px] max-[800px]:mt-4 min-w-[100px] max-[450px]:m-auto max-[450px]:mt-3">
-                        <button
-                            class="w-12 h-10 flex items-center justify-center ml-4 rounded-lg max-[800px]:w-8 max-[800px]:h-8 max-[800px]:ml-2">
-                            <svg width="28px" height="28px" stroke-width="2.5" viewBox="0 0 24 24" fill="none"
-                                xmlns="http://www.w3.org/2000/svg" color="#000000">
-                                <path
-                                    d="M3 17h18M6 10V3.6a.6.6 0 01.6-.6h10.8a.6.6 0 01.6.6V10m3 10.4V14a4 4 0 00-4-4H7a4 4 0 00-4 4v6.4a.6.6 0 00.6.6h16.8a.6.6 0 00.6-.6z"
-                                    stroke="#1B1C30" stroke-width="2.5"></path>
-                                <path d="M17 13.01l.01-.011" stroke="#1B1C30" stroke-width="2.5" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
-                            </svg>
-                        </button>
-                        <button
-                            class="w-12 h-10 flex items-center justify-center ml-4 rounded-lg max-[800px]:w-8 max-[800px]:h-8 max-[800px]:ml-2 max-[450px]:mx-8">
-                            <svg width="24px" height="24px" stroke-width="2.5" viewBox="0 0 24 24" fill="none"
-                                xmlns="http://www.w3.org/2000/svg" color="#000000">
-                                <path d="M7 6h10M7 9h10M9 17h6" stroke="#1B1C30" stroke-width="2.5" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
-                                <path
-                                    d="M3 12h-.4a.6.6 0 00-.6.6v8.8a.6.6 0 00.6.6h18.8a.6.6 0 00.6-.6v-8.8a.6.6 0 00-.6-.6H21M3 12V2.6a.6.6 0 01.6-.6h16.8a.6.6 0 01.6.6V12M3 12h18"
-                                    stroke="#1B1C30" stroke-width="2.5"></path>
-                            </svg>
-                        </button>
-                        <button id="btnadd" type="button"
-                            class="w-20 h-10 flex items-center justify-center mx-4 font-bold rounded-lg max-[800px]:w-8 max-[800px]:h-8 max-[800px]:ml-2 max-[450px]:ml-0">
-                            <svg width="24px" height="24px" stroke-width="2.5" viewBox="0 0 24 24" fill="none"
-                                xmlns="http://www.w3.org/2000/svg" color="#FFFFFF">
-                                <path
-                                    d="M8 12h4m4 0h-4m0 0V8m0 4v4M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
-                                    stroke="#FFFFFF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+    <div class="principal mt-6">
+        <!--Componente para cargar el menu superior del formulario-->
+        <MenuUsuarioDashboard class="mr-8" />
+        <div class="mdprincipal flex-col mt-8 px-8 overflow-hidden">
+            <div class="h-16 w-full rounded-xl flex justify-between items-center content-buttons max-[450px]:flex-wrap">
+                <form action="" class="w-3/4 flex items-center h-full mt-4 max-[500px]:w-full">
+                    <input type="text" class="rounded-lg relative w-2/4 h-12 outline-none max-[800px]:w-full min-w-[200px]"
+                        placeholder="Buscar... (usuario/correo)" v-model="buscar.buscador" @keyup="buscarUsuarios()">
+                    <div class="flex justify-end items-center">
+                        <button class="absolute mr-4"><svg width="20px" height="20px" stroke-width="2" viewBox="0 0 24 24"
+                                fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+                                <path d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"
+                                    stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 </path>
                             </svg>
                         </button>
                     </div>
+                </form>
+                <div
+                    class="buttons flex mt-4 mr-[-15px] max-[800px]:mt-4 min-w-[100px] max-[450px]:m-auto max-[450px]:mt-3">
+                    <button
+                        class="w-12 h-10 flex items-center justify-center ml-4 rounded-lg max-[800px]:w-8 max-[800px]:h-8 max-[800px]:ml-2">
+                        <svg width="28px" height="28px" stroke-width="2.5" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" color="#000000">
+                            <path
+                                d="M3 17h18M6 10V3.6a.6.6 0 01.6-.6h10.8a.6.6 0 01.6.6V10m3 10.4V14a4 4 0 00-4-4H7a4 4 0 00-4 4v6.4a.6.6 0 00.6.6h16.8a.6.6 0 00.6-.6z"
+                                stroke="#1B1C30" stroke-width="2.5"></path>
+                            <path d="M17 13.01l.01-.011" stroke="#1B1C30" stroke-width="2.5" stroke-linecap="round"
+                                stroke-linejoin="round"></path>
+                        </svg>
+                    </button>
+                    <button
+                        class="w-12 h-10 flex items-center justify-center ml-4 rounded-lg max-[800px]:w-8 max-[800px]:h-8 max-[800px]:ml-2 max-[450px]:mx-8">
+                        <svg width="24px" height="24px" stroke-width="2.5" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" color="#000000">
+                            <path d="M7 6h10M7 9h10M9 17h6" stroke="#1B1C30" stroke-width="2.5" stroke-linecap="round"
+                                stroke-linejoin="round"></path>
+                            <path
+                                d="M3 12h-.4a.6.6 0 00-.6.6v8.8a.6.6 0 00.6.6h18.8a.6.6 0 00.6-.6v-8.8a.6.6 0 00-.6-.6H21M3 12V2.6a.6.6 0 01.6-.6h16.8a.6.6 0 01.6.6V12M3 12h18"
+                                stroke="#1B1C30" stroke-width="2.5"></path>
+                        </svg>
+                    </button>
+                    <button id="btnadd" type="button"
+                        class="w-20 h-10 flex items-center justify-center mx-4 font-bold rounded-lg max-[800px]:w-8 max-[800px]:h-8 max-[800px]:ml-2 max-[450px]:ml-0">
+                        <svg width="24px" height="24px" stroke-width="2.5" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" color="#FFFFFF">
+                            <path
+                                d="M8 12h4m4 0h-4m0 0V8m0 4v4M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+                                stroke="#FFFFFF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+                            </path>
+                        </svg>
+                    </button>
                 </div>
-                <p class="text-black text-lg"></p>
-                <div class="line bg-slate-800 h-0.5 mt-4 w-full min-w-[200px]"></div>
-                <div class="h-screen">
-                    <p class="font-extrabold text-slate-900 mt-8 ml-4 max-[425px]:mt-16">{{ usuarios.length }}<span
-                            class="text-gray-500 font-normal ml-2">registro encontrado!</span></p>
-                    <div class="tables overflow-y-scroll h-4/6">
-                        <TablesUsuarios :dataUsers="usuarios" />
-                        <div class="flex justify-center mt-6">
-                            <TailwindPagination
-                                :item-classes="['text-gray-500', 'rounded-full', 'border-none', 'ml-1', 'hover:bg-gray-200']"
-                                :active-classes="['text-white', 'rounded-full', 'bg-purpleLogin']" :limit="1"
-                                :keepLength="true" :data="data" @pagination-change-page="pagina = $event" />
-                        </div>
+            </div>
+            <p class="text-black text-lg"></p>
+            <div class="line bg-slate-800 h-0.5 mt-4 w-full min-w-[200px]"></div>
+            <div class="h-screen">
+                <p class="font-extrabold text-slate-900 mt-8 ml-4 max-[425px]:mt-16">{{ usuarios.length }}<span
+                        class="text-gray-500 font-normal ml-2">registro encontrado!</span></p>
+                <div class="tables overflow-y-scroll h-4/6">
+                    <TablesUsuarios :dataUsers="usuarios" />
+                    <div class="flex justify-center mt-6">
+                        <TailwindPagination
+                            :item-classes="['text-gray-500', 'rounded-full', 'border-none', 'ml-1', 'hover:bg-gray-200']"
+                            :active-classes="['text-white', 'rounded-full', 'bg-purpleLogin']" :limit="1" :keepLength="true"
+                            :data="data" @pagination-change-page="pagina = $event" />
                     </div>
                 </div>
             </div>
@@ -194,7 +190,8 @@ async function buscarUsuarios() {
 #btnadd {
     background-color: #1b1c30;
 }
-.tables::-webkit-scrollbar{
-    width:5px;
+
+.tables::-webkit-scrollbar {
+    width: 5px;
 }
 </style>
