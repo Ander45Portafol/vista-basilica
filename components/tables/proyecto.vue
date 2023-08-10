651d0b78555d809e2fe37416fd2b39638ca811a9
itsRoberOOF <83309227+itsRoberOOF@users.noreply.github.com>
2023-08-10 15:35:24 -0600

    Lightbox en proyectos

--- components/tables/proyecto.vue
+++ components/tables/proyecto.vue
@@ -4,7 +4,8 @@
             class="data-contained flex justify-between mt-4 rounded-xl p-4 max-[400px]:flex-wrap max-[400px]:w-full min-w-[200px]">
             <div class="flex justify-start w-3/4 items-center max-[400px]:w-full">
                 <img :src="iconos_url + proyecto.icono_proyecto"
-                    class="h-10 w-10 rounded-lg border-2 border-gray-800 max-[400px]:hidden" />
+                    @click="mostrarLightBox(iconos_url + proyecto.icono_proyecto)"
+                    class="h-10 w-10 cursor-pointer rounded-lg border-2 border-gray-800 max-[400px]:hidden" />
                 <div
                     class="datainfo flex-col ml-8 max-[400px]:p-0 max-[400px]:w-full max-[400px]:ml-0 max-[400px]:text-center">
                     <p class="font-extrabold text-xl text-salte-900 max-[750px]:text-[18px]">
@@ -70,6 +71,7 @@
             </div>
         </div>
     </div>
+    <vue-easy-lightbox :visible="visibleRef" :imgs="imgsRef" :index="indexRef" @hide="esconderLightBox"></vue-easy-lightbox>
     <!-- Proyet modal -->
     <div id="staticModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
         class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -395,6 +397,7 @@
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import { Modal } from 'flowbite'
+import VueEasyLightbox from 'vue-easy-lightbox'
 import validaciones from '../../assets/validaciones.js';
 const props = defineProps({
     dataProyectos: Array,
@@ -947,6 +950,23 @@ function falseIconoBorrarIcono() {
     if (iconoPreview.value) {
         mostrarIconoBorrarIcono.value = false;
     }
+}
+
+const visibleRef = ref(false);
+const indexRef = ref(0);
+const imgsRef = ref([]);
+
+function abrirLightBox() {
+    visibleRef.value = true;
+}
+
+function mostrarLightBox(url) {
+    imgsRef.value = url;
+    abrirLightBox();
+}
+
+function esconderLightBox(){
+    visibleRef.value = false
 }
 
 </script>
