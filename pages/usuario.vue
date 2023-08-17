9148ce08b4c2bbbdd11eb8cc6c819533fd257a60
Ander45Portafol <anderdeco25@gmail.com>
2023-08-17 14:55:58 -0600

    cambios usuarios

--- pages/usuario.vue
+++ pages/usuario.vue
@@ -65,13 +65,44 @@
             <p class="text-black text-lg"></p>
             <div class="line bg-slate-800 h-0.5 mt-4 w-full min-w-[200px]"></div>
             <div class="h-screen">
-                <p class="font-extrabold text-slate-900 mt-8 ml-4 max-[425px]:mt-16">{{ usuarios.length }}<span
-                        class="text-gray-500 font-normal ml-2">registro encontrado!</span></p>
+                <p v-if="usuarios" class="font-extrabold text-slate-900 mt-8 ml-4 max-[425px]:mt-16">{{ usuarios.length
+                }}<span class="text-gray-500 font-normal ml-2">registro encontrado!</span></p>
+                <p v-else class="font-extrabold text-slate-900 mt-8 ml-4 max-[425px]:mt-16">
+                    -
+                    <span class="text-gray-500 font-normal ml-2">registros encontrados!</span>
+                </p>
                 <div class="tables overflow-y-scroll h-3/5 pr-4">
-                    <TablesUsuarios :dataUsers="usuarios" :actualizarData="leerUsuarios" />
+                    <div v-if="!usuarios" class="loadingtable overflow-hidden h-full pr-4">
+                        <div class="contained-data flex-col" v-for="number in 6" :key="number">
+                            <div
+                                class="border-4 border-slate-300 animate-pulse flex justify-between mt-4 rounded-xl p-4 max-[400px]:flex-wrap max-[400px]:w-full min-w-[200px]">
+                                <div class="flex justify-start w-3/4 items-center max-[400px]:w-full">
+                                    <div class="h-16 w-16 bg-slate-300 mr-5 rounded-2xl max-[600px]:hidden"></div>
+                                    <div class="datainfo flex-col max-[400px] p-0 w-full ml-0 mt-2 text-center">
+                                        <div
+                                            class="h-4 bg-slate-300 rounded-full dark:bg-gray-700 w-48 max-[450px]:w-40 max-[400px]:w-full mb-4">
+                                        </div>
+                                        <div
+                                            class="h-3 bg-slate-300 rounded-full dark:bg-gray-700 w-1/2 mb-2.5 max-[400px]:w-full">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div
+                                    class="buttons-data flex justify-center items-center max-[750px]:flex-col max-[400px]:flex-row max-[400px]:m-auto max-[400px]:mt-2">
+                                    <div
+                                        class="bg-slate-300 h-10 w-10 ml-4 rounded-md flex items-center justify-center max-[750px]:ml-0 max-[750px]:mt-2 max-[400px]:mt-0 max-[400px]:ml-2">
+                                    </div>
+                                    <div
+                                        class="bg-slate-300 h-10 w-10 ml-4 rounded-md flex items-center justify-center max-[750px]:ml-0 max-[750px]:mt-2 max-[400px]:mt-0 max-[400px]:ml-8">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <TablesUsuarios v-if="usuarios" :dataUsers="usuarios" :actualizarData="leerUsuarios" />
                 </div>
                 <div class="flex justify-center mt-6">
-                    <TailwindPagination
+                    <TailwindPagination v-if="usuarios"
                         :item-classes="['text-gray-500', 'rounded-full', 'border-none', 'ml-1', 'hover:bg-gray-200']"
                         :active-classes="['text-white', 'rounded-full', 'bg-purpleLogin']" :limit="1" :keepLength="true"
                         :data="data" @pagination-change-page="pagina = $event" />
@@ -89,6 +120,7 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 definePageMeta({
     layout: "principal",
+    middleware: "middleware-paginas",
 });
 //Seccion para cargar o modificar el DOM despues de haber cargado todo el template
 onMounted(() => {
@@ -113,6 +145,8 @@ onMounted(() => {
             modal.hide();
         });
     }
+    token.value = localStorage.getItem('token');
+    leerUsuarios();
 });
 async function generarReporte() {
     try {
@@ -146,9 +180,10 @@ function visibilidadRegistros() {
     }
 }
 //Seccion para establecer variables
+const token = ref(null);
 const data = ref(null);
 const pagina = ref(useRoute().query.pagina || 1);
-let usuarios = computed(() => data.value.data);
+let usuarios = computed(() => data.value?.data);
 const buscar = ref({
     buscador: "",
 })
@@ -167,17 +202,25 @@ const Toast = Swal.mixin({
 async function leerUsuarios() {
     try {
         if (registros_visibles.value) {
-            const { data: res } = await axios.get(`/usuarios?page=${pagina.value}`);
-            return data.value = res;
+            const { data: res } = await axios.get(`/usuarios?page=${pagina.value}`, {
+                headers: {
+                    Authorization: `Bearer ${token.value}`,
+                },
+            });
+            data.value = res;
+            console.log(usuarios.value);
         } else {
-            const { data: res } = await axios.get(`/usuarios_ocultos?page=${pagina.value}`);
-            return data.value = res;
+            const { data: res } = await axios.get(`/usuarios_ocultos?page=${pagina.value}`, {
+                headers: {
+                    Authorization: `Bearer ${token.value}`,
+                },
+            });
+            data.value = res;
         }
     } catch (error) {
         console.log(error);
     }
 }
-await leerUsuarios();
 watch(pagina, async () => {
     //Se evalua si el buscador tiene algún valor para ver si se realiza el leer o el buscar
     if (buscar.value.buscador != "") {
@@ -195,11 +238,23 @@ async function buscarUsuarios() {
         //Se evalua que el buscador no este vacio
         if (buscar.value.buscador != "") {
             if (registros_visibles.value) {
-                const { data: res } = await axios.get(`/usuarios_search?page=${pagina.value}`);
-                return data.value = res;
+                const { data: res } = await axios.get(
+                    `/usuarios_search?page=${pagina.value}&buscador=${buscar.value.buscador}`, {
+                    headers: {
+                        Authorization: `Bearer ${token.value}`,
+                    },
+                }
+                );
+                data.value = res;
             } else {
-                const { data: res } = await axios.get(`/usuarios_search_ocultos?page=${pagina.value}`);
-                return data.value = res;
+                const { data: res } = await axios.get(
+                    `/usuarios_search_ocultos?page=${pagina.value}&buscador=${buscar.value.buscador}`, {
+                    headers: {
+                        Authorization: `Bearer ${token.value}`,
+                    },
+                }
+                );
+                data.value = res;
             }
             // Actualiza la URL con el parámetro de página
             useRouter().push({ query: { pagina: pagina.value } });
