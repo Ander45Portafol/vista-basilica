fc7480005965fcb24a5ad9e7d390713c7754da68
itsRoberOOF <83309227+itsRoberOOF@users.noreply.github.com>
2023-08-23 09:49:53 -0600

    graficas generales comentadas y estandarizadas

--- middleware/middleware_login.js
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    if (process.client) {
-        if (localStorage.getItem('token')) {
-            window.location.href = '/principal';
-        }
-    }
-});
