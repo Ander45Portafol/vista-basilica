005975e0657a5d6700ab2ad105e6d1286109b998
Theguishe <guillermogacc0526@gmail.com>
2023-08-24 07:29:13 -0600

    responsive graficas

--- components/tables/proyecto.vue
+++ components/tables/proyecto.vue
@@ -21,50 +21,55 @@
                     </p>
                 </div>
             </div>
-            <div class="buttons-data flex justify-center items-center max-[750px]:flex-col max-[400px]:flex-row max-[400px]:m-auto max-[400px]:mt-2"
+            <div class="buttons-data flex justify-center flex-row items-center max-[750px]:flex-col max-[400px]:flex-row max-[400px]:m-auto max-[400px]:mt-2 max-[400px]:flex-wrap"
                 v-if="proyecto.campos.visibilidad_proyecto == 1">
-                <button class="h-10 w-10 rounded-md flex items-center justify-center mr-4 imagenbtn max-[750px]:mr-0"
-                    @click="modalImagenes(proyecto.id)">
-                    <svg width="26px" height="26px" stroke-width="2" viewBox="0 0 24 24" fill="none"
-                        xmlns="http://www.w3.org/2000/svg" color="#000000">
-                        <path d="M21 7.6v12.8a.6.6 0 01-.6.6H7.6a.6.6 0 01-.6-.6V7.6a.6.6 0 01.6-.6h12.8a.6.6 0 01.6.6z"
-                            stroke="#3F4280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path
-                            d="M18 4H4.6a.6.6 0 00-.6.6V18M7 16.8l5.444-1.8L21 18M16.5 13a1.5 1.5 0 110-3 1.5 1.5 0 010 3z"
-                            stroke="#3F4280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </button>
-                <button class="h-10 w-10 rounded-md flex items-center justify-center mr-4 reportbtn" v-if="proyecto.campos.visibilidad_proyecto == 1" @click="generarReporteProyecto(proyecto.id)">
-                    <svg width="26px" height="26px" viewBox="0 0 24 24"
-                        stroke-width="2" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
-                        <path
-                            d="M4 21.4V2.6a.6.6 0 01.6-.6h11.652a.6.6 0 01.424.176l3.148 3.148A.6.6 0 0120 5.75V21.4a.6.6 0 01-.6.6H4.6a.6.6 0 01-.6-.6zM8 10h8M8 18h8M8 14h4"
-                            stroke="#7AAB97" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path d="M16 2v3.4a.6.6 0 00.6.6H20" stroke="#7AAB97" stroke-width="2.5" stroke-linecap="round"
-                            stroke-linejoin="round"></path>
-                    </svg>
-                </button>
-                <button
-                    class="h-10 w-10 rounded-md flex items-center justify-center editbtn max-[400px]:mx-4 max-[750px]:mt-2"
-                    @click="estadoActualizar(proyecto.id)" v-if="proyecto.campos.visibilidad_proyecto == 1">
-                    <svg width="26px" height="26px" stroke-width="2" viewBox="0 0 24 24" fill="none"
-                        xmlns="http://www.w3.org/2000/svg" color="#000000">
-                        <path
-                            d="M3 21h18M12.222 5.828L15.05 3 20 7.95l-2.828 2.828m-4.95-4.95l-5.607 5.607a1 1 0 00-.293.707v4.536h4.536a1 1 0 00.707-.293l5.607-5.607m-4.95-4.95l4.95 4.95"
-                            stroke="#C99856" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </button>
-                <button
-                    class="h-10 w-10 rounded-md flex items-center justify-center ml-4 deletebtn max-[750px]:ml-0 max-[750px]:mt-2 max-[400px]:mt-0 max-[400px]:mx-4"
-                    @click="borrarProyecto(proyecto.id, proyecto.campos.nombre_proyecto)"
-                    v-if="proyecto.campos.visibilidad_proyecto == 1">
-                    <svg width="26px" height="26px" viewBox="0 0 24 24" stroke-width="2" fill="none"
-                        xmlns="http://www.w3.org/2000/svg" color="#000000">
-                        <path
-                            d="M20 9l-1.995 11.346A2 2 0 0116.035 22h-8.07a2 2 0 01-1.97-1.654L4 9M21 6h-5.625M3 6h5.625m0 0V4a2 2 0 012-2h2.75a2 2 0 012 2v2m-6.75 0h6.75"
-                            stroke="#872727" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </button>
+                <div class="flex">
+                    <button class="h-10 w-10 rounded-md flex items-center justify-center mr-4 imagenbtn max-[750px]:mr-0 max-[750px]:mt-2"
+                        @click="modalImagenes(proyecto.id)">
+                        <svg width="26px" height="26px" stroke-width="2" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" color="#000000">
+                            <path d="M21 7.6v12.8a.6.6 0 01-.6.6H7.6a.6.6 0 01-.6-.6V7.6a.6.6 0 01.6-.6h12.8a.6.6 0 01.6.6z"
+                                stroke="#3F4280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                            <path
+                                d="M18 4H4.6a.6.6 0 00-.6.6V18M7 16.8l5.444-1.8L21 18M16.5 13a1.5 1.5 0 110-3 1.5 1.5 0 010 3z"
+                                stroke="#3F4280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg>
+                    </button>
+                    <button class="h-10 w-10 rounded-md flex items-center justify-center mr-4 reportbtn max-[750px]:mt-2 max-[750px]:m-0"
+                        v-if="proyecto.campos.visibilidad_proyecto == 1" @click="generarReporteProyecto(proyecto.id)">
+                        <svg width="26px" height="26px" viewBox="0 0 24 24" stroke-width="2" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" color="#000000">
+                            <path
+                                d="M4 21.4V2.6a.6.6 0 01.6-.6h11.652a.6.6 0 01.424.176l3.148 3.148A.6.6 0 0120 5.75V21.4a.6.6 0 01-.6.6H4.6a.6.6 0 01-.6-.6zM8 10h8M8 18h8M8 14h4"
+                                stroke="#7AAB97" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                            <path d="M16 2v3.4a.6.6 0 00.6.6H20" stroke="#7AAB97" stroke-width="2.5" stroke-linecap="round"
+                                stroke-linejoin="round"></path>
+                        </svg>
+                    </button>
+                </div>
+                <div class="flex">
+                    <button
+                        class="h-10 w-10 rounded-md flex items-center justify-center editbtn max-[400px]:mx-4 max-[750px]:mt-2 max-[400px]:ml-12"
+                        @click="estadoActualizar(proyecto.id)" v-if="proyecto.campos.visibilidad_proyecto == 1">
+                        <svg width="26px" height="26px" stroke-width="2" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" color="#000000">
+                            <path
+                                d="M3 21h18M12.222 5.828L15.05 3 20 7.95l-2.828 2.828m-4.95-4.95l-5.607 5.607a1 1 0 00-.293.707v4.536h4.536a1 1 0 00.707-.293l5.607-5.607m-4.95-4.95l4.95 4.95"
+                                stroke="#C99856" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg>
+                    </button>
+                    <button
+                        class="h-10 w-10 rounded-md flex items-center justify-center ml-4 deletebtn max-[750px]:ml-0 max-[750px]:mt-2 max-[400px]:mt-0 max-[400px]:mx-4 max-[400px]:mr-12"
+                        @click="borrarProyecto(proyecto.id, proyecto.campos.nombre_proyecto)"
+                        v-if="proyecto.campos.visibilidad_proyecto == 1">
+                        <svg width="26px" height="26px" viewBox="0 0 24 24" stroke-width="2" fill="none"
+                            xmlns="http://www.w3.org/2000/svg" color="#000000">
+                            <path
+                                d="M20 9l-1.995 11.346A2 2 0 0116.035 22h-8.07a2 2 0 01-1.97-1.654L4 9M21 6h-5.625M3 6h5.625m0 0V4a2 2 0 012-2h2.75a2 2 0 012 2v2m-6.75 0h6.75"
+                                stroke="#872727" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg>
+                    </button>
+                </div>
             </div>
             <div class="buttons-data flex justify-center items-center max-[750px]:flex-col max-[400px]:flex-row max-[400px]:m-auto max-[400px]:mt-2"
                 v-else>
@@ -460,7 +465,8 @@
 .image_loader {
     border: 3px solid #FFF;
 }
-.reportbtn{
+
+.reportbtn {
     border: 3px solid #7AAB97;
 }
 </style>
@@ -484,11 +490,11 @@ onMounted(() => {
 });
 
 const token = ref(null);
-const id=ref(null);
+const id = ref(null);
 
 async function generarReporteProyecto(id_proyecto) {
     //Constante donde se almacena la respuesta que retorna de la api
-    const ruta = new URL(`http://127.0.0.1:8000/api/proyecto_reporte/`+id_proyecto);
+    const ruta = new URL(`http://127.0.0.1:8000/api/proyecto_reporte/` + id_proyecto);
     //Le añadimos el token en la ruta del reporte
     ruta.searchParams.append('token', token.value);
     //Le añadimos el id del usuario que ha iniciado sesion, se captura mediante el token
