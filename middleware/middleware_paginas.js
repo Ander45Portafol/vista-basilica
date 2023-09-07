32a8a480b5027267c1a9d1ff52f4548fd9000ae3
itsRoberOOF <83309227+itsRoberOOF@users.noreply.github.com>
2023-09-07 15:42:50 -0600

    avance middleware

--- middleware/middleware_paginas.js
+++ middleware/middleware_paginas.js
@@ -1,9 +1,41 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+import axios from "axios";
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const trueValuesArray = [];
     if (process.client) {
         if (!localStorage.getItem('token')) {
             return navigateTo('/');
-        }else{
-            
+        } else {
+            try {
+                const res = await axios.post('/verificar-token/', localStorage.getItem('token'), {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    },
+                })
+
+                if (res.data.message == 'El token de acceso es valido.') {
+                    const res = await axios.post("/accesos-usuario", localStorage.getItem('token'), {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('token')}`,
+                        },
+                    });
+
+                    for (const key in res.data.data.data[0]) {
+                        if (res.data.data.data[0].hasOwnProperty(key) && res.data.data.data[0][key] === true) {
+                            trueValuesArray.push(key);
+                        }
+                    }
+
+                    if (to.path === '/pagina' && trueValuesArray.includes('acceso_navegabilidad')) {
+                        window.location.href = '/principal';
+                    }
+                } else {
+                    return navigateTo('/');
+                }
+
+            }catch (error) {
+            console.log(error);
         }
     }
+}
 });
