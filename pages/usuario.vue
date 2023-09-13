e41be728f3aa808e2e68fd68f2bfe44b58b19d93
Kanzi11 <20210312@ricaldone.edu.sv>
2023-09-13 09:16:25 -0600

    Buscador de usuarios

--- pages/usuario.vue
+++ pages/usuario.vue
@@ -6,7 +6,7 @@
             <div class="h-16 w-full rounded-xl flex justify-between items-center content-buttons max-[450px]:flex-wrap">
                 <form action="" class="w-3/4 flex items-center h-full mt-4 max-[500px]:w-full">
                     <input type="text" class="rounded-lg relative w-2/4 h-12 outline-none max-[800px]:w-full min-w-[200px]"
-                        placeholder="Buscar... (usuario/correo)" v-model="buscar.buscador" @keyup="buscarUsuarios()">
+                        placeholder="Buscar... (usuario/correo)" v-model="buscar.buscador" @keyup="buscarUsuarios($event)">
                     <div class="flex justify-end items-center">
                         <button class="absolute mr-4" type="button" @click="limpiarBuscador()"><svg width="20px"
                                 height="20px" stroke-width="2" viewBox="0 0 24 24" fill="none"
@@ -247,48 +247,61 @@ watch(pagina, async () => {
     //Se cambia la url para agregar en que pagina se encuentra el usuario
     useRouter().push({ query: { pagina: pagina.value } })
 })
-async function buscarUsuarios() {
+
+
+//Constante ref para controlar que no se pueda spamear el delete en el buscador y bugear el token
+const ejecutado_despues_borrar = ref(false);
+
+//Función para buscar registros dependiendo del valor del buscador
+async function buscarUsuarios(event) {
     try {
         //Se evalua que el buscador no este vacio
         if (buscar.value.buscador != "") {
 
+            //Se coloca como false para que si se pueda presionar el borrar
+            ejecutado_despues_borrar.value = false;
+
             //Se actualiza la ruta del navegador para mostrar lo que se esta buscando
             useRouter().push({ query: { buscador: buscar.value.buscador } });
 
-            //Se filtran los registros de data según los parámetros del buscador (nombre_pagina / numero_pagina)
-            data.value = data.value.filter(usuario =>
-                usuario.campos.usuario.toLowerCase().includes(buscar.value.buscador.toLowerCase()) ||
-                usuario.campos.correo_usuario.toLowerCase().includes(buscar.value.buscador.toLowerCase())
+            //Se filtran los registros de data según los parámetros del buscador (usuario,correo_usuario )
+            const data_filtrada = ref();
+            
+            data_filtrada.value = data.value.filter(usuario =>
+            usuario.campos.usuario.toLowerCase().includes(buscar.value.buscador.toLowerCase()) ||
+            usuario.campos.correo_usuario.toLowerCase().includes(buscar.value.buscador.toLowerCase())
             );
 
             //Se limpia el array de registros paginados
             usuarios.value = [];
 
             //Se evalua la longitud del array filtrado, si es 0 significa que no hay registros similares
-            if (data.value.length == 0) {
+            if (data_filtrada.value.length == 0) {
                 //Se actualiza el valor de la constante de búsqueda a true para mostrar un mensaje al usuario
                 ceroRegistrosEncontrados.value = true;
             } else {
                 //En caso de que si hayan registros similares, se paginan los registros de 10 en 10 usando el for
-                for (let i = 0; i < data.value.length; i += 10) {
-                    usuarios.value.push(data.value.slice(i, i + 10));
+                for (let i = 0; i < data_filtrada.value.length; i += 10) {
+                    usuarios.value.push(data_filtrada.value.slice(i, i + 10));
                 }
                 //Se actualiza el valor de la constante de búsqueda a false
                 ceroRegistrosEncontrados.value = false;
             }
 
         } else {
-            //Se regresa a la página 1 y se cargan todos los registros
-            pagina.value = 1;
-            leerUsuarios();
-            useRouter().push({ query: { pagina: pagina.value } });
-            //Se actualiza el valor de la constante de búsqueda a false
-            ceroRegistrosEncontrados.value = false;
+            //Se valida las teclas que el usuario puede presionar para bugear el buscador
+            if (buscar.value.buscador.length == 0 && (event.key != 'CapsLock' && event.key != 'Shift' && event.key != 'Control' && event.key != 'Alt' && event.key != 'Meta' && event.key != 'Escape' && event.key != 'Enter') && !ejecutado_despues_borrar.value) {
+                 //Se coloca como true para que no se pueda presionar el borrar
+                ejecutado_despues_borrar.value = true;
+                //Se regresa a la página 1 y se cargan todos los registros
+                limpiarBuscador();
+                //Se actualiza el valor de la constante de búsqueda a false
+                ceroRegistrosEncontrados.value = false;
+            }
         }
     } catch (error) {
         console.log(error);
-
-        //Se muestra un sweetalert con el mensaje
+        //Se muestra un sweetalert con el mensaje   
         Swal.fire({
             icon: "error",
             title: "Error",
@@ -297,6 +310,7 @@ async function buscarUsuarios() {
         });
     }
 }
+
 
 //Función para limpiar el buscador
 function limpiarBuscador() {
