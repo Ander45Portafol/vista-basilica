e3051bf300ef3bc8db1148859190e7490df8757f
Ander45Portafol <anderdeco25@gmail.com>
2023-09-20 20:10:27 -0600

    subiendo cambios

--- pages/recuperacion.vue
+++ pages/recuperacion.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="contained h-screen w-screen flex overflow-hidden">
-        <div class="w-full mt-20 ml-10 flex-col">
+    <div class="contained h-screen w-screen flex overflow-hidden flex-col">
+        <div class="flex justify-start ml-10 mt-8"> 
+            <NuxtLink to="/" class="bg-darkSpace h-14 w-44 rounded-2xl flex justify-center items-center text-white gap-3 text-2xl font-bold">
+                <svg width="30px" height="30px" stroke-width="2" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg" color="#FFF">
+                    <path
+                        d="M16 12H8m0 0l3.5 3.5M8 12l3.5-3.5M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+                        stroke="#FFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>Volver
+            </NuxtLink>
+        </div>
+        <div class="w-full h-5/6 mt-4 ml-10 flex-col">
             <h1 class="text-white font-extrabold text-5xl text-center">¡Recupera tu contraseña!</h1>
             <div class="w-full h-full flex justify-center">
                 <form class="card bg-space h-3/4 w-2/4 mt-10 rounded-3xl" id="cardUsuario"
                     @submit.prevent="validarCredenciales">
                     <div class="flex-col w-full">
                         <div class="flex justify-center text-center">
-                            <div class="flex-col text-white p-8 w-2/4">
+                            <div class="flex-col text-white p-8 w-3/4">
                                 <p class="font-bold text-2xl mt-4">Datos del usuario: </p>
                                 <p class="font-normal text-base mt-4">El usuario que desea recuperar su clave
                                     necesita brindar primero sus datos para
